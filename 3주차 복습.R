4fdbfe2c426d253e1f13c25b4636ea48c8d01919
Choseoyoung <seoyoung0000@naver.com>
2021-09-16 18:10:56 +0900

    Week3

---
+++ 3주차 복습.R
@@ -0,0 +1,14 @@
+#built-in functions
+factorial(5)
+abs(-3)
+log(16,2)
+
+install.packages('cowsay')
+cowsay::say("function is fun","random")
+
+scores<-c(80,95,74,83,60,98,81,77,79,94)
+mean(scores)
+sqrt(var(scores)) #what is it?
+sort(scores,True)
+hist(scores)
+
